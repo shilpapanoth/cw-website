04670e948ed09ea197174d97021024a76f4e0c87
shilpapanoth <shilpap.mec@gmail.com>
2017-04-27 14:39:03 +0530

    adding bower_components as static folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 app = express();
 app.use(express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/bower_components'));
 app.listen(3000, function () {
   console.log('Server started at :::: 3000');
 });
