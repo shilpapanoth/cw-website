62bc0302017a8a7ca66df53a552915853c114543
shilpapanoth <shilpap.mec@gmail.com>
2017-04-28 12:32:29 +0530

    app list and details page

---
+++ public/js/app-detail-controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('cwApp')
+    .controller('appDetailController', appDetailController);
+
+  appDetailController.$inject = ['$scope', '$rootScope'];
+
+  function appDetailController ($scope, $rootScope) {
+    
+  }
+
+}) ()
---
+++ public/js/app-list-controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('cwApp')
+    .controller('appListControler', appListControler);
+
+  appListControler.$inject = ['$scope', '$rootScope'];
+
+  function appListControler ($scope, $rootScope) {
+    
+  }
+
+}) ()
