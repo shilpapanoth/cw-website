81b950d4ee3518d9414d13714896b78b9e79fcee
SriDivyaT <sridivya.tirunagari@incture.com>
2017-05-04 15:30:44 +0530

    font and about page

--- public/js/about-us-controller.js
+++ public/js/about-us-controller.js
@@ -3,15 +3,39 @@
 	angular
 	.module('cwApp')
 	.controller('aboutController',aboutController);
-	aboutController.$inject = ['$scope', '$rootScope', '$anchorScroll', '$location'];
+	aboutController.$inject = ['$scope', '$rootScope', '$anchorScroll', '$location', '$http'];
 
-	function aboutController ($scope, $rootScope, $anchorScroll, $location){
+	function aboutController ($scope, $rootScope, $anchorScroll, $location, $http){
 		$rootScope.contactNav="";
     $scope.gotoBottom = gotoBottom;
+    getBenefits();
+    getStatistics();
 
     function gotoBottom () {
-      $location.hash('aboutText');
+      $location.hash('aboutDetail');
       $anchorScroll();
+    }
+
+    function getBenefits (){
+    	$http({
+		    method: 'GET',
+		    url: '/json/benefits.json'
+	  	}).then(function(data){
+	  		$scope.benefits = data.data.benefits;
+	  	},function(error) {
+
+	  	});
+    }
+
+    function getStatistics (){
+    	$http({
+		    method: 'GET',
+		    url: '/json/statistics.json'
+	  	}).then(function(data){
+	  		$scope.statistics = data.data.statistics;
+	  	},function(error) {
+
+	  	});
     }
 
 	}
