2330bcc41909dffbcc9a95af3a730a1b4a1fa438
SriDivyaT <sridivya.tirunagari@incture.com>
2017-05-14 20:47:17 +0530

    carousel swipe

--- public/js/app-detail-controller.js
+++ public/js/app-detail-controller.js
@@ -16,6 +16,19 @@
     $('#appCarousel').carousel({
       interval: 3000
     });
+
+    $(".carousel-inner").swipe( {
+            //Generic swipe handler for all directions
+            swipeLeft:function(event, direction, distance, duration, fingerCount) {
+              $(this).parent().carousel('prev');
+            },
+            swipeRight: function() {
+              $(this).parent().carousel('next');
+            },
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold:0
+    });
+
     if(!$rootScope.appData){
     	getAppData();
     }else{
