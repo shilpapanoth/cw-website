66b10ef209eb0066354a99d341ab8378366e09c2
SriDivyaT <sridivya.tirunagari@incture.com>
2017-05-14 20:18:01 +0530

    swipe

--- public/js/home-controller.js
+++ public/js/home-controller.js
@@ -11,12 +11,17 @@
     $('#mainCarousel').carousel({
       interval: 3500
     });
-    $("#mainCarousel").swiperight(function() {
-    		  $(this).carousel('prev');
-	  });
-		$("#mainCarousel").swipeleft(function() {
-		      $(this).carousel('next');
-	  });
+    $(".carousel-inner").swipe( {
+						//Generic swipe handler for all directions
+						swipeLeft:function(event, direction, distance, duration, fingerCount) {
+							$(this).parent().carousel('prev');
+						},
+						swipeRight: function() {
+							$(this).parent().carousel('next');
+						},
+						//Default is 75px, set to 0 for demo so any distance triggers swipe
+						threshold:0
+		});
 	}
 })()
 
